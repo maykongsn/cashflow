0a3b6521c624f21732e60c2c3f4d8e89bcaf66d9
maykongsn <contatomaykon@hotmail.com>
2023-04-19 17:23:16 -0300

    feat: configuração inicial do projeto

---
+++ .eslintrc.cjs
@@ -0,0 +1,68 @@
+module.exports = {
+  env: {
+    es2021: true,
+    browser: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "no-shadow": "off",
+    "@typescript-eslint/no-shadow": ["error"],
+    rules: {
+      "no-param-reassign": [
+        "error",
+        { props: true, ignorePropertyModificationsFor: ["draft"] },
+      ],
+    },
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: [
+          "test.{ts,tsx}",
+          "test-*.{ts,tsx}",
+          "**/*{.,_}{test,spec}.{ts,tsx}",
+          "**/jest.config.ts",
+          "**/jest.setup.ts",
+        ],
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        singleQuote: false,
+        tabWidth: 2,
+        arrowParens: "always",
+      },
+    ],
+    semi: ["error", "always"],
+    camelcase: "off",
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+    "import/no-unresolved": "off",
+    "no-dupe-keys": "off",
+    "no-unused-vars": "off",
+    "import/extensions": "off",
+    "no-useless-constructor": "off",
+    "no-empty-function": "off",
+    "max-classes-per-file": "off",
+    "consistent-return": "off",
+  },
+};
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  white: "#FFFFFF",
+  black: "#010101",
+
+  "gray-100": "#F0F2F1",
+  "gray-300": "#2C2E2F",
+  "gray-400": "#242529",
+  "gray-500": "#161819",
+  "gray-600": "#121214",
+
+  "green-100": "#00EEAF",
+  "green-300": "#00B7SF",
+  "green-400": "#015F43",
+
+  "red-100": "#F75A68",
+  "red-400": "AA2834",
+} as const;
